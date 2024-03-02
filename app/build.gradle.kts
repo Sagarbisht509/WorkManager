8bff8f23d595ce1e1b3b65b45ded85970a72cb37
sagarbisht509 <sagarbisht509@gmail.com>
2024-03-03 00:46:16 +0530

    Implemented UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,5 +69,5 @@ dependencies {
 
     implementation ("androidx.work:work-runtime-ktx:2.9.0")
     implementation ("androidx.compose.runtime:runtime-livedata:1.6.2")
-    implementation ("io.coil-kt:coil:2.6.0")
+    implementation ("io.coil-kt:coil-compose:2.6.0")
 }
--- app/src/main/java/com/sagar/workmanager/MainActivity.kt
+++ app/src/main/java/com/sagar/workmanager/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sagar.workmanager
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
@@ -8,19 +9,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.work.Constraints
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
+import coil.compose.AsyncImage
 import com.sagar.workmanager.ui.theme.WorkManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,6 +63,7 @@ class MainActivity : ComponentActivity() {
                     }
                 }
 
+                ScreenContent(viewModel)
             }
         }
     }
@@ -85,3 +96,48 @@ class MainActivity : ComponentActivity() {
         workManager.enqueue(workRequest)
     }
 }
+
+@Composable
+fun ScreenContent(viewModel: PhotoViewModel) {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceAround) {
+        viewModel.uncompressedUri?.let { uri ->
+            ImageWithHeading(
+                heading = "Uncompressed Image",
+                uri = uri
+            )
+        }
+        viewModel.compressedBitmap?.let { bitmap ->
+            ImageWithHeading(
+                heading = "Compressed Image",
+                bitmap = bitmap
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageWithHeading(
+    heading: String = "",
+    uri: Uri? = null,
+    bitmap: Bitmap? = null
+) {
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = heading)
+        Spacer(modifier = Modifier.height(5.dp))
+        if (uri != null) {
+            AsyncImage(model = uri, contentDescription = heading, modifier = Modifier.fillMaxWidth().height(400.dp))
+        } else {
+            Image(bitmap = bitmap!!.asImageBitmap(), contentDescription = heading, modifier = Modifier.fillMaxWidth().height(400.dp))
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
